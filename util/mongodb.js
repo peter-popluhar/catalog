b0328202012134525a0e41f31f4d79130ce7736c
Peter Popluhar <peter.popluhar@socialbakers.com>
2021-02-01 21:20:34 +0100

    run prettier on all files

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,17 +1,17 @@
-import { MongoClient } from 'mongodb'
+import {MongoClient} from 'mongodb'
 
-const { MONGODB_URI, MONGODB_DB } = process.env
+const {MONGODB_URI, MONGODB_DB} = process.env
 
 if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
+	throw new Error(
+		'Please define the MONGODB_URI environment variable inside .env.local'
+	)
 }
 
 if (!MONGODB_DB) {
-  throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local'
-  )
+	throw new Error(
+		'Please define the MONGODB_DB environment variable inside .env.local'
+	)
 }
 
 /**
@@ -22,27 +22,27 @@ if (!MONGODB_DB) {
 let cached = global.mongo
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null }
+	cached = global.mongo = {conn: null, promise: null}
 }
 
 export async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn
-  }
+	if (cached.conn) {
+		return cached.conn
+	}
 
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+	if (!cached.promise) {
+		const opts = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
 
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
-      return {
-        client,
-        db: client.db(MONGODB_DB),
-      }
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
+		cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+			return {
+				client,
+				db: client.db(MONGODB_DB),
+			}
+		})
+	}
+	cached.conn = await cached.promise
+	return cached.conn
 }
