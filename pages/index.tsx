185ae725a7bc079aa175f9f9cda4e84a60d28f6f
Peter Popluhar <peter.popluhar@socialbakers.com>
2021-02-01 20:47:20 +0100

    add Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,84 @@
 import Head from 'next/head'
-import { connectToDatabase } from '../util/mongodb'
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import {connectToDatabase} from '../util/mongodb'
+import {GetStaticProps, GetStaticPaths, GetServerSideProps} from 'next'
 
-export default function Home({ isConnected }) {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({isConnected}) {
+	return (
+		<div className='container'>
+			<Head>
+				<title>Create Next App</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
+			<main>
+				<h1 className='title'>
+					Welcome to <a href='https://nextjs.org'>Next.js with MongoDB!</a>
+				</h1>
 
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
+				{isConnected ? (
+					<h2 className='subtitle'>You are connected to MongoDB</h2>
+				) : (
+					<h2 className='subtitle'>
+						You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+						for instructions.
+					</h2>
+				)}
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+				<p className='description'>
+					Get started by editing <code>pages/index.js</code>
+				</p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+				<div className='grid'>
+					<a href='https://nextjs.org/docs' className='card'>
+						<h3>Documentation &rarr;</h3>
+						<p>Find in-depth information about Next.js features and API.</p>
+					</a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+					<a href='https://nextjs.org/learn' className='card'>
+						<h3>Learn &rarr;</h3>
+						<p>Learn about Next.js in an interactive course with quizzes!</p>
+					</a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+					<a
+						href='https://github.com/vercel/next.js/tree/master/examples'
+						className='card'
+					>
+						<h3>Examples &rarr;</h3>
+						<p>Discover and deploy boilerplate example Next.js projects.</p>
+					</a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+					<a
+						href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+						className='card'
+					>
+						<h3>Deploy &rarr;</h3>
+						<p>
+							Instantly deploy your Next.js site to a public URL with Vercel.
+						</p>
+					</a>
+				</div>
+			</main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-    </div>
-  )
+			<footer>
+				<a
+					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Powered by{' '}
+					<img src='/vercel.svg' alt='Vercel Logo' className='logo' />
+				</a>
+			</footer>
+		</div>
+	)
 }
 
 export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase()
+	const {client} = await connectToDatabase()
 
-  const isConnected = await client.isConnected()
+	const isConnected = await client.isConnected()
 
-  return {
-    props: { isConnected },
-  }
+	return {
+		props: {isConnected},
+	}
 }
