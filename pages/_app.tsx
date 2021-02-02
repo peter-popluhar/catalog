9377d0464c9f2b494446a5790ec94f87b34a096b
Peter Popluhar <peter.popluhar@socialbakers.com>
2021-02-02 22:03:12 +0100

    add css module with scss import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import './../styles/index.scss'
+import c from './../styles-modules/container.module.scss'
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />
+	return (
+		<div className={c.container}>
+			<Component {...pageProps} />
+		</div>
+	)
 }
 export default MyApp
