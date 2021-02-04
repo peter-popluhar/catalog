49a46645d50b678a31e73d76462b7b9d305e7ecf
Peter Popluhar <peter.popluhar@socialbakers.com>
2021-02-04 21:33:14 +0100

    active link in navigation

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,22 +1,42 @@
 import Link from 'next/link'
 import {navigationCopy} from './../../copy/navigation'
+import {useLanguageContext} from './../../context/language-context'
+import styles from './navigation.module.scss'
+import {useRouter} from 'next/router'
+import cslx from 'clsx'
 
 export default function Navigation() {
-	const lng = 'en'
+	const router = useRouter()
+	const pathname = router.pathname
+	const {lng} = useLanguageContext()
 
 	const copyPath = navigationCopy?.[lng]
 
 	return (
-		<nav>
-			<ul>
-				<li>
+		<nav className={styles.nav}>
+			<ul className={styles.list}>
+				<li className={styles.item}>
 					<Link href='/items'>
-						<a>{copyPath.allItems}</a>
+						<a
+							className={cslx(
+								styles.link,
+								pathname === '/items' && styles.active
+							)}
+						>
+							{copyPath.allItems}
+						</a>
 					</Link>
 				</li>
-				<li>
+				<li className={styles.item}>
 					<Link href='/add'>
-						<a>{copyPath.addItem}</a>
+						<a
+							className={cslx(
+								styles.link,
+								pathname === '/add' && styles.active
+							)}
+						>
+							{copyPath.addItem}
+						</a>
 					</Link>
 				</li>
 			</ul>
