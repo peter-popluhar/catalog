7e78daf163494dc66a49d1ed90e892c890a4b71b
Peter Popluhar <peter.popluhar@socialbakers.com>
2021-02-06 20:46:27 +0100

    add progress bar for loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,20 @@ import type {AppProps} from 'next/app'
 import './../styles/index.scss'
 import Layout from './../components/layout'
 import {SettingsProvider} from './../context/settings-context'
+import NProgress from 'nprogress'
+import Router from 'next/router'
 
 function MyApp({Component, pageProps}: AppProps) {
+	Router.events.on('routeChangeStart', () => {
+		NProgress.start()
+	})
+	Router.events.on('routeChangeComplete', () => {
+		NProgress.done()
+	})
+	Router.events.on('routeChangeError', () => {
+		NProgress.done()
+	})
+
 	return (
 		<>
 			<Head>
